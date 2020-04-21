3d058b040229604610b0a58958fa91c4dae8a0c5
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2020-04-22 01:15:08 +0800

    Fix issues in setup script.

--- server_setup.py
+++ server_setup.py
@@ -14,8 +14,8 @@ def file_replacer(filename, replace_rules):
 
 def main():
     file_replacer(appsettings, [('port=3333', 'port=3306'),('localhost:5000','darrendanielday.club')])
-    file_replacer(dbinit, [('context.Database.EnsureDeleted();','')])
-    file_replacer(startup,[('Version(8, 0, 3)','Version(5, 7, 29)')] )
+    # file_replacer(dbinit, [('context.Database.EnsureDeleted();','')])
+    file_replacer(startup,[('Version(8, 0, 3)','Version(5, 7, 29)'),('DbInitializer.Init(this);','')] )
 
 if __name__ == "__main__":
     main()
