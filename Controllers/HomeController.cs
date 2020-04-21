4b62ecf4a5e23bbd15e6fb688826bb504a0a6666
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2020-04-22 00:51:20 +0800

    Add fake homepage.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XueLeMeBackend.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View("HomePage");
+        }
+    }
+}
